7518d47bed1719f0defecf8d707f0e26a0fa0d6c
Lyndon Sanche <lsanche@lyndeno.ca>
2023-02-18 16:55:49 -0700

    Initial commit based on gtk-rs book

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      naersk-lib = naersk.lib."${system}";
+    in rec {
+      packages.illuminator = naersk-lib.buildPackage {
+        pname = "gnome-smbios";
+        root = ./.;
+        nativeBuildInputs = [ pkgs.gtk4 pkgs.pkg-config ];
+      };
+      packages.default = packages.gnome-smbios;
+
+      apps.gnome-smbios = utils.lib.mkApp {
+        drv = packages.gnome-smbios;
+      };
+      apps.default = apps.gnome-smbios;
+
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [ rustc cargo gtk4 pkg-config ];
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+    });
+}
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Button};
+
+const APP_ID: &str = "org.lyndeno.gnome-smbios";
+
+fn main() -> glib::ExitCode {
+    println!("Hello, world!");
+
+    // Create app
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to activate signal
+    app.connect_activate(build_ui);
+
+    // Run app
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    // Create a button
+    let button = Button::builder()
+        .label("Press")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // Connect to "clicked" signal of button
+    button.connect_clicked(|button| {
+        // set the label to "Hello" after click
+        button.set_label("Hello");
+    });
+
+    // Create window and set title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("GTK SMBIOS")
+        .child(&button)
+        .build();
+
+    //Present
+    window.present();
+}
